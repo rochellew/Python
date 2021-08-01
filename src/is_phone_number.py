af800b6ebc979d96b032cdab560e45115705f9bf
rochellew <32746298+rochellew@users.noreply.github.com>
2021-08-01 12:55:18 -0400

    Regex practice

---
+++ src/is_phone_number.py
@@ -0,0 +1,25 @@
+def is_phone_number(text):
+    if len(text) != 12:
+        return False
+    for i in range (0,3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range (4,7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range (8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+print('Is 415-555-4242 a phone number?')
+print(is_phone_number('415-555-4242'))
+print()
+print('Is Moshi moshi a phone number?')
+print(is_phone_number('Moshi moshi'))
+
+        
