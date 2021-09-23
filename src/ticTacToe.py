070b5ca4b0be6040c11db5cb0f76be7170c3fb3c
William Rochelle <rochellew@etsu.edu>
2021-09-22 23:39:00 -0400

    Update ticTacToe.py

--- src/ticTacToe.py
+++ src/ticTacToe.py
@@ -5,11 +5,11 @@ theBoard = {'top-L': '', 'top-M': '', 'top-R': '',
             'low-L': '', 'low-M': '', 'low-R': ''}
 
 def printBoard(board):
-    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print(board['top-L'] + ' |' + board['top-M'] + ' |' + board['top-R'])
     print('-+-+-')
-    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print(board['mid-L'] + ' |' + board['mid-M'] + ' |' + board['mid-R'])
     print('-+-+-')
-    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+    print(board['low-L'] + ' |' + board['low-M'] + ' |' + board['low-R'])
 
 turn='X'
 for i in range(9):
